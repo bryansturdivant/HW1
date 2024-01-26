105b87edea051847f6fb9bfe5528c88cb9205c04
bryansturdivant <157143446+bryansturdivant@users.noreply.github.com>
2024-01-26 11:20:39 -0500

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,13 @@
+//File name must be the same as the class name
+
+public class Car {
+    public String make; // public makes the variable public - You can also make private
+    public String manufacturer;
+    public boolean handicap;
+
+    public Car(String make, String manufacturer, boolean handicap){
+        make = make;
+
+    }
+
+}
