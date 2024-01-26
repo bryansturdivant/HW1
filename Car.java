94bdaaa92d1697008c91c1a3641e3b40f2640781
bryansturdivant <157143446+bryansturdivant@users.noreply.github.com>
2024-01-26 11:34:10 -0500

    blah

--- Car.java
+++ Car.java
@@ -6,7 +6,10 @@ public class Car {
     public boolean handicap;
 
     public Car(String make, String manufacturer, boolean handicap){
-        make = make;
+        this.make = make; //like self in python - Tells java that we want to refer to something that belongs to the object in question
+        //Take this object make, and make it the variable that we passed in 
+        this.manufacturer = manufacturer;
+        this.handicap = handicap;
 
     }
 
---
+++ ParkingSpot.java
@@ -0,0 +1,10 @@
+public class ParkingSpot {
+    public Car parkedCar;//Car is now a data type 
+    public boolean handicap;
+
+    public ParkingSpot(boolean handicap){ //name it the same as the class 
+        this.handicap = handicap;
+
+    }
+    
+}
