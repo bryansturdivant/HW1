fd5e4c61b439341268857343d3b31e8efa7015d4
bryansturdivant <157143446+bryansturdivant@users.noreply.github.com>
2024-01-26 11:51:27 -0500

    meh

--- ParkingLot.java
+++ ParkingLot.java
@@ -1,5 +1,24 @@
 public class ParkingLot {
+    public ParkingSpot[] spots;
 
-    
-    
+    public ParkingLot() {
+        this.spots = new ParkingSpot[10];
+        for (int i = 0; i < 10; i++) {
+            if (i <= 1) {
+                this.spots[i] = new ParkingSpot(true);
+
+            } else {
+                this.spots[i] = new ParkingSpot(false);
+            }
+
+        }
+
+    }
+
+    public String toString() {
+
+        return "";// Returns a string
+
+    }
+
 }
